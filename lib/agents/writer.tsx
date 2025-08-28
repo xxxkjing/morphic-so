6aaf0c7107d342a41fcbb826ff8f5cc1c8a8b8e4
MetaIllusion <xkjing@outlook.com>
2025-08-28 20:16:20 +0800

    Update writer.tsx

--- lib/agents/writer.tsx
+++ lib/agents/writer.tsx
@@ -15,14 +15,14 @@ export async function writer(
   uiStream.append(answerSection)
 
   const openai = createOpenAI({
-    baseURL: process.env.SPECIFIC_API_BASE,
-    apiKey: process.env.SPECIFIC_API_KEY,
+    baseURL: process.env.OPENAI_API_BASE,
+    apiKey: process.env.OPENAI_API_KEY,
     organization: '' // optional organization
   })
 
   await streamText({
-    model: openai!.chat(process.env.OPENAI_API_MODEL || 'llama3-70b-8192'),
-    maxTokens: 2500,
+    model: openai!.chat('gemini-2.5-flash-lite-preview-06-17'),
+    maxTokens: 1000,
     system: `As a professional writer, your job is to generate a comprehensive and informative, yet concise answer of 400 words or less for the given question based solely on the provided search results (URL and content). You must only use information from the provided search results. Use an unbiased and journalistic tone. Combine search results together into a coherent answer. Do not repeat text. If there are any images relevant to your answer, be sure to include them as well. Aim to directly address the user's question, augmenting your response with insights gleaned from the search results. 
     Whenever quoting or referencing information from a specific URL, always cite the source URL explicitly. Please match the language of the response to the user's language.
     Always answer in Markdown format. Links and images must follow the correct format.
