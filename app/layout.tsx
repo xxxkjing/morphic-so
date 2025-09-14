20f6b71a45989f58fd2762958fedebf05b7046a2
MetaIllusion <xkjing@outlook.com>
2025-09-14 18:28:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Search'
+const title = 'Xia So'
 const description =
   'A fully open-source AI-powered answer engine with a generative UI.'
 
